432b269f3d202e7c98847762164ab6892508e672
Kimberly Wang <59712863+kimberly332@users.noreply.github.com>
2025-03-17 08:23:47 -0400

    Revert any changes from ORC. (Latest version)

--- src/pages/AddMerchantPage.js
+++ src/pages/AddMerchantPage.js
@@ -1,8 +1,7 @@
 // src/pages/AddMerchantPage.js
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import MerchantInputForm from '../components/merchants/MerchantInputForm';
-import EnhancedOcrMerchantForm from '../components/merchants/EnhancedOcrMerchantForm';
+import MerchantInputForm from '../components/merchants/MerchantInputForm';
 import { checkUserAuth } from '../firebase/userAuth';
 import { checkUserHasSubmittedToday, deleteMerchant } from '../firebase/firestore';
 import SuccessNotification from '../components/common/SuccessNotification';
@@ -182,7 +181,7 @@ function AddMerchantPage() {
       <p className="description">
         分享你今天所遇到的商人資訊，幫助其他玩家。
       </p>
-      <EnhancedOcrMerchantForm />
+      <MerchantInputForm />
       
       {/* 成功提示訊息 */}
       {showNotification && (
